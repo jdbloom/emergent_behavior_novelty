0dc556b4ef54fc145381a632558194cbe623548a
Dominic Cupo <Dominic.Cupo@Gmail.com>
2019-04-18 17:58:19 -0400

    Changed to extent CBuzzLoopFunctions not just CLoopFunctions to allow for Buzzful uses

--- loop_functions/mpga_loop_functions/mpga_loop_functions.h
+++ loop_functions/mpga_loop_functions/mpga_loop_functions.h
@@ -2,6 +2,7 @@
 #define MPGA_LOOP_FUNCTIONS_H
 
 #include <argos3/core/simulator/loop_functions.h>
+#include <buzz/argos/buzz_loop_functions.h>
 
 using namespace argos;
 
@@ -11,42 +12,42 @@ using namespace argos;
  * Any experiment that uses CMPGA must also have loop functions that
  * inherit from this class.
  */
-class CMPGALoopFunctions : public CLoopFunctions {
-   
+class CMPGALoopFunctions : public CBuzzLoopFunctions {
+
 public:
 
-   CMPGALoopFunctions();
+    CMPGALoopFunctions();
 
-   /**
-    * Class destructor.
-    */
-   virtual ~CMPGALoopFunctions() {}
+    /**
+     * Class destructor.
+     */
+    virtual ~CMPGALoopFunctions() {}
 
-   /**
-    * Configures the trial using the passed genome.
-    * @param pf_genome The genome.
-    */
-   virtual void ConfigureFromGenome(const Real* pf_genome) = 0;
+    /**
+     * Configures the trial using the passed genome.
+     * @param pf_genome The genome.
+     */
+    virtual void ConfigureFromGenome(const Real *pf_genome) = 0;
 
-   /**
-    * Returns the current trial.
-    */
-   virtual UInt32 GetTrial() const;
+    /**
+     * Returns the current trial.
+     */
+    virtual UInt32 GetTrial() const;
 
-   /**
-    * Sets the current trial.
-    * @param un_trial The trial number.
-    */
-   virtual void SetTrial(UInt32 un_trial);
+    /**
+     * Sets the current trial.
+     * @param un_trial The trial number.
+     */
+    virtual void SetTrial(UInt32 un_trial);
 
-   /**
-    * Returns the score associated to the current trial.
-    */
-   virtual Real Score() = 0;
+    /**
+     * Returns the score associated to the current trial.
+     */
+    virtual Real Score() = 0;
 
 private:
 
-   UInt32 m_unTrial;
+    UInt32 m_unTrial;
 
 };
 
